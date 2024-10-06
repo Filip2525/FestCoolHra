bb475acc2b42a175fc696dceef6841ee2795e734
Filip2525 <filip.stefko2525@gmail.com>
2024-10-06 11:17:43 +0200

    Created Menus

---
+++ festcoolhra/src/MenuSignalManager.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_music_toggled(toggled_on: bool) -> void:
+	Variables.music = toggled_on
+func _on_sounds_toggled(toggled_on: bool) -> void:
+	Variables.sounds = toggled_on
+func _on_volume_value_changed(value: float) -> void:
+	Variables.volume =  value / 100
--- festcoolhra/src/coin.gd
+++ festcoolhra/src/coin.gd
@@ -1,12 +1,4 @@
 extends Node2D
-
-var animation
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	animation = $AnimationPlayer
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var animation = $AnimationPlayer
 func _process(delta):
 	animation.play("IdleCoin")
