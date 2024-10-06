3b9574b2917105f06c8727eb7ae20f3286440e34
Filip2525 <filip.stefko2525@gmail.com>
2024-10-06 11:51:06 +0200

    added level2

--- festcoolhra/src/MenuSignalManager.gd
+++ festcoolhra/src/MenuSignalManager.gd
@@ -8,3 +8,5 @@ func _on_sounds_toggled(toggled_on: bool) -> void:
 	Variables.sounds = toggled_on
 func _on_volume_value_changed(value: float) -> void:
 	Variables.volume =  value / 100
+func _on_exit_to_desktop_pressed() -> void:
+	get_tree().quit()
